535ced234f15b948ff54f1d7a451f78e3fd36665
Anshu Kumar <anshu.kumar@ebizontek.com>
2018-10-27 19:59:34 +0530

    api base code

--- index.js
+++ index.js
@@ -1,42 +1,49 @@
 var express = require('express'),
     path = require('path'),
     nodeMailer = require('nodemailer'),
-    bodyParser = require('body-parser'),
+    formidable = require('formidable'),
     keys = require('./config/keys');
 var port = 3000;
 var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
 app.get('/', function (req, res) {
   res.render('index');
 });
 
 app.post('/send-email', function (req, res) {
-let transporter = nodeMailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-        user: keys.username,
-        pass: keys.password
-    }
-});
-let mailOptions = {
-      from: '"Krunal Lathiya" <xx@gmail.com>', // sender address
-      to: req.body.to, // list of receivers
-      subject: req.body.subject, // Subject line
-      html: req.body.body // html body
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          return console.log(error);
-      }
-      console.log('Message %s sent: %s', info.messageId, info.response);
-          res.render('index');
+  var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+     let transporter = nodeMailer.createTransport({
+          host: 'smtp.gmail.com',
+          port: 465,
+          secure: true,
+          auth: {
+              user: keys.username,
+              pass: keys.password
+          }
       });
+      let mailOptions = {
+            from: fields.from + '<'+keys.username+'>', // sender address
+            to: keys.to, // list of receivers
+            subject: fields.subject, // Subject line
+            html: fields.body, // html body
+            attachments: [
+              {   // utf-8 string as an attachment
+                  filename: files.data.name,
+                  path: files.data.path
+              }
+            ]
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                res.send("Something went wrong Try After Sometime")
+            }
+            console.log('Message %s sent: %s', info.messageId, info.response);
+                res.send("Your resume have been sent sucessfully")
+            });
+    });
   });
 app.listen(port, function(){
             console.log('Server is running at port: ',port);
